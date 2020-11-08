d13e47085c4bb127862f73b93e8cda4908505378
spc009 <supatcha1@hotmail.com>
2020-11-08 16:42:31 +0700

    add database

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('npm scriptis read')
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('./database/CHOPPEE.db',(err)=>{
+
+    if(err){
+        console.log(err.message)
+    }
+    console.log('connected to CHOPPEE db in sqlite')
+
+    db.close((err)=>{
+        if(err){
+            console.log(err.message)
+        }
+    })
+    console.log('CHOPPEE db is closed')
+});
