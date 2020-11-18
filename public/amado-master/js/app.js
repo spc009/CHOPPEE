42c593de0a57e3c11d353005383357b8ebbd0b4b
Anthocerophyta <love.tom.100.mag@gmail.com>
2020-11-18 19:02:23 +0700

    min test

--- public/amado-master/js/app.js
+++ public/amado-master/js/app.js
@@ -1301,7 +1301,6 @@ function AddToCart(orderNumber,Name,price, pdCode, num ,n){
         "productCode": pdCode,
         "qty": num
     };
-    
     // NumberCart();
     $.ajaxSetup({
         headers: {
